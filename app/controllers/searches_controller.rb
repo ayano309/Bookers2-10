3ef7e3492affa9c17bcd2e9e035ca88a8b6a5593
ayano309 <ayatomato.k@gmail.com>
2022-05-05 06:25:34 +0000

    作成したタグで検索ができる

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,14 +1,19 @@
 class SearchesController < ApplicationController
   before_action :authenticate_user!
 
-  def search 
+  def search
     @range = params[:range]
+    @search = params[:search]
+    @word = params[:word]
+
     if @range == "User"
-      @users = User.looks(params[:search], params[:word])
-    else
-      @books = Book.looks(params[:search], params[:word])
+      @users = User.looks(@search,@word)
+    elsif @range == "Book"
+      @books = Book.looks(@search,@word)
+    elsif @range == "Tag"
+      @books = Tag.search_books_for(@search,@word)
     end
 
   end
-  
+
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,4 +3,20 @@ class Tag < ApplicationRecord
   has_many :books, through: :tagbooks
   
   validates :name, uniqueness: true, presence: true
+  
+  
+  def self.search_books_for(search, word)
+    
+    if search == "perfect_match"
+      tags = Tag.where(name: word)
+    elsif search == "forward_match"
+      tags = Tag.where('name LIKE ?', word + '%')
+    elsif search == "backward_match"
+      tags = Tag.where('name LIKE ?', '%' + word)
+    elsif search == "partial_match"
+      tags = Tag.where('name LIKE ?', '%' + word + '%')
+    end
+    # injectはたたみ込み演算
+    return tags.inject(init = []) {|result, tag| result + tag.books}
+  end
 end
